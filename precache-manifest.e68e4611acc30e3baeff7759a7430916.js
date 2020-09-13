3a8e16edf2596a5c48d65206279668860f3964f7
Sofia Lee <sofia819@gmail.com>
2020-09-13 01:00:18 -0400

    Updates

---
+++ precache-manifest.e68e4611acc30e3baeff7759a7430916.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63bed1b0bf21849d3b507581d3231906",
+    "url": "/card_flipping_game/index.html"
+  },
+  {
+    "revision": "3ccb2b97b5be3bed73bd",
+    "url": "/card_flipping_game/static/css/main.8e4f1c47.chunk.css"
+  },
+  {
+    "revision": "b8375a1b0151d3df3734",
+    "url": "/card_flipping_game/static/js/2.fd2fba32.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/card_flipping_game/static/js/2.fd2fba32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ccb2b97b5be3bed73bd",
+    "url": "/card_flipping_game/static/js/main.6294541c.chunk.js"
+  },
+  {
+    "revision": "b0f7067db106a112cbae",
+    "url": "/card_flipping_game/static/js/runtime-main.d6d12936.js"
+  }
+]);
